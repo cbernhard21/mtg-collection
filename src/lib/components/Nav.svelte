fcd4bcea9a29dfbeb89f92dfba95b04d2c9b6ad8
Christopher <chris_bernhard@yahoo.com>
2022-10-10 14:54:22 -0500

    working on nav

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -3,10 +3,19 @@
     <li class="nav-item"><a href="/dashboard">Dashboard</a></li>
     <li class="nav-item"><a href="/dashboard/search">Search</a></li>
     <li class="nav-item"><a href="/dashboard/collection">Collection</a></li>
+    <li class="nav-item"><a href="/dashboard">Profile</a></li>
+    <li class="nav-item"><a href="/">Log Out</a></li>
   </ul>
 </nav>
 
 <style>
+
+  nav {
+    background-color: var(--bg-nav);
+    height: 100%;
+    padding: 2rem 1rem 0 2rem;
+    text-align: right;
+  }
   
   a {
     text-decoration: none;
@@ -17,7 +26,7 @@
   ul {
     display: flex;
     flex-direction: column;
-    gap: 2rem;
+    gap: 3rem;
   }
 
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  import '$lib/css/global.css';
+</script>
+
 <h1>Welcome to the app</h1>
 
 <a href="/dashboard">Enter If You Dare</a>
